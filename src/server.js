3cb6a5dad9b5a077ad0e36bda6dbc89f8cc3e2ae
hraya <hraya@lssu.edu>
2019-09-11 18:14:51 -0500

    Completed lesson-1

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,40 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+// middleware
+// const log = (req, res, next) => {
+//   console.log('logging')
+//   next()
+// }
+
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+app.router('/api', router)
+
+app.get('/data', (req, res) => {
+  res.send({ data: [1, 2, 3] })
+})
+
+app.put('/data', (req, res) => {})
+
+app.post('/data', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Server is on 3000')
+  })
+}
+
+// app.get('/', (req, res) => {
+//     res.send({message: 'hello'})
+// })
+
+// app.post('/', (req, res) => {
+//     console.log(req.body)
+//     res.send({message: 'ok'})
+// })
