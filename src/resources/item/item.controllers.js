aec3b86c1d7a42a430542d5689da5ca6cb12054e
hraya <hraya@lssu.edu>
2019-09-14 01:08:29 -0500

    Lesson 2 complete

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,22 @@
-export default {}
+const getAll = (req, res) => {
+  res.send({ message: 'I am an item get' })
+}
+
+const add = (req, res) => {
+  res.send({ message: 'I am an item post' })
+}
+
+const update = (req, res) => {
+  res.send({ message: 'I am an item put' })
+}
+
+const remove = (req, res) => {
+  res.send({ message: 'I am an item delete' })
+}
+
+export default {
+  getAll,
+  add,
+  update,
+  remove
+}
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,23 @@
 import { Router } from 'express'
+// import controllers from './item.controllers'
+
+const controller = (req, res) => {
+  res.send({ message: 'hello' })
+}
 
 const router = Router()
+
+// /api/item
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+// /api/item/:id
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
