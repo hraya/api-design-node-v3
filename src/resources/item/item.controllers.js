34e011fe7bc1ce089c3025d9aa55e58c8c16f5fc
hraya <hraya@lssu.edu>
2019-09-14 01:05:25 -0500

    Lesson 3 practice and Notes

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,27 @@
-export default {}
+import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
+
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+  const item = await Item.create({
+    name: 'Clean up',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+  const remove = await Item.findByIdAndRemove(item._id).exec()
+
+  console.log(remove)
+  //   console.log(item)
+}
+
+// export default {}
+// console.log(await Item.find({}).exec()) for Read
+// const updated = await Item.findByIdAndUpdate(
+//     item._id,
+//     { name: 'eat' },
+//     { new: true }
+//   ).exec()
+
+//   console.log(updated) for update
+run()
